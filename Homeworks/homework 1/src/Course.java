f630ae8854a01f53d8d5c25b61f7b0d94ba250f8
alexander_khilchenko <kh.git@mail.ru>
2021-11-28 10:51:31 +1000

    ДЗ № 1: Полоса препятсвий с тестами (без логики прохождения командой)

--- Homeworks/homework 1/src/Course.java
+++ Homeworks/homework 1/src/Course.java
@@ -10,6 +10,8 @@
  *
  */
 
+import java.util.Random;
+import java.util.Arrays;
 
 public class Course {
 
@@ -37,9 +39,49 @@ public class Course {
         // вывод в строку - для теста создания, ну и так: пригодится…
         @Override
         public String toString() {
-            return "Испытание {" + endurance +"}";
+            return "Препятствие (" + endurance +")";
         }
     }
 
+    /*
+    для унификации процесса сделаем любую полосу из 10 препятсвий
+     */
+    private Trial[] trialArray = new Trial[10];
+
+    public Course() {
+        // конструктор случайной полосы
+        for (int i = 0; i < 10; i += 1) {
+            trialArray[i] = new Trial();
+        }
+    }
+
+    public Course(int[]array) {
+        // конструктор «заказной» полосы.
+        // полосу передаем как двумерный массив, в котором строка - параметры
+        // полосы, кроме того,
+        // будем считать, что массив всегда передается «правильным» и не станем
+        // обрабатывать исключений.
+        for (int i = 0; i < 10; i += 1) {
+            trialArray[i] = new Trial(array[i]);
+        }
+    }
+
+    // простейшие тесты работоспособности полученного класса.
+    public static void main(String[] args) {
+        // тест создания случайной полосы препятствий
+        Course course = new Course();
+        System.out.println(Arrays.toString(course.trialArray));
+
+        // тест создания заказной полосы препятствий
+        int array[] = {17,10,7,4,20,18,11,13,14,18};
+        Course course1 = new Course(array);
+        System.out.println(Arrays.toString(course1.trialArray));
+
+        /* вывод тестов :
+        [Испытание (10), Испытание (14), Испытание (17), Испытание (1), Испытание (13), Испытание (12), Испытание (4), Испытание (5), Испытание (8), Испытание (14)]
+        [Испытание (17), Испытание (10), Испытание (7), Испытание (4), Испытание (20), Испытание (18), Испытание (11), Испытание (13), Испытание (14), Испытание (18)]
+        */
+
+    }
 
 }
